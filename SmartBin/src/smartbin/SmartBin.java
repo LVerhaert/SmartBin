b2d1f0c9e74385ae90f603403e2d4650ca84d422
Liza <lvmverha@avans.nl>
2019-12-10 14:36:14 +0100

    verwerkAfval testen

--- SmartBin/src/smartbin/SmartBin.java
+++ SmartBin/src/smartbin/SmartBin.java
@@ -23,9 +23,9 @@ public class SmartBin {
         Data data = new Data();
 //        SerialConnector.execute();
 
-//        while (true) {
-//            verwerkAfval(data);
-//        }
+        while (true) {
+            verwerkAfval(data);
+        }
         /**
          * Liza
          */
@@ -34,7 +34,7 @@ public class SmartBin {
         /**
          * Duygu
          */
-        KleurSensor.execute(9600);
+//        KleurSensor.execute(9600);
         /**
          * Ketura
          */
